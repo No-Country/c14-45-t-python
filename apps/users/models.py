0a5f45df39815f8e4e73174f345442414b00ee6e
David Urroz <davidurrozp@gmail.com>
2023-10-10 13:30:28 -0600

    Pequeño avance de Login con JWT por parte de backend

--- apps/users/models.py
+++ apps/users/models.py
@@ -10,7 +10,8 @@ class User(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField()
     password = models.CharField(max_length=50)
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True)
+    is_verified = models.BooleanField(default=False)
 
 
 class Rol(models.Model):
