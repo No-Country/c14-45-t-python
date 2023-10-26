fee7602039cb99ceed90cdb157eb8dd2bbf61705
eseminara <eze.seminara@gmail.com>
2023-10-26 15:42:05 -0300

    feat:
    login & user proudcts

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,6 +66,20 @@ MIDDLEWARE = [
 ]
 
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    
+]
+
+
+LOGIN_URL = 'login'
+
+LOGIN_REDIRECT_URL = 'login_success'
+
+
+AUTH_USER_MODEL = 'users.User'
+
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 
@@ -73,7 +88,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Ruta a la carpeta de plantillas
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
